7e4c819295e64ba4748bf7bf4a2cc5d2b5ce4e59
oaioa <thomas.viola@insa-lyon.fr>
2017-11-23 14:19:20 +0000

    HELO working

--- basicChatServerHaskell/src/Main.hs
+++ basicChatServerHaskell/src/Main.hs
@@ -47,13 +47,10 @@ runConn (sock, _) chan msgNum = do
     let broadcast msg = writeChan chan (msgNum, msg)
     hdl <- socketToHandle sock ReadWriteMode
     hSetBuffering hdl NoBuffering
---    name <- fmap init (hGetLine hdl) 
   --  case stripPrefix "HELO" name of
     --        Just restOfString -> hPutStrLn hdl("HELO "++restOfString++"\nIP:"++show ip++"\nPort:"++show port++"\nStudentID:17342676\n")
       --      Nothing -> do
-	--		broadcast ("--> " ++ name ++ " entered chat.")
-   		--	hPutStrLn hdl ("Welcome, " ++ name ++ "!") 
-   
+	--		   
     commLine <- dupChan chan
  
     -- fork off a thread for reading from the duplicated channel
@@ -65,15 +62,21 @@ runConn (sock, _) chan msgNum = do
     handle (\(SomeException _) -> return ()) $ fix $ \loop -> do
         line <- fmap init (hGetLine hdl)
         case stripPrefix "HELO" line of
-            Just restOfString -> hPutStrLn hdl("HELO "++restOfString++"\nIP:"++show ip++"\nPort:"++show port++"\nStudentID:17342676\n")
+            Just restOfString -> hPutStrLn hdl("HELO "++restOfString++"\nIP:"++show ip++"\nPort:"++show port++"\nStudentID:17342676\n") >>loop
             Nothing -> return () 
+        case stripPrefix "JOIN" line of
+            Just restOfString -> hPutStrLn hdl("Joined !")>>loop
+            Nothing -> return () 
+
         case line of
              -- If an exception is caught, send a message and break the loop
-             "KILL_SERVICE" -> hPutStrLn hdl "Bye!"
              "kill" -> hPutStrLn hdl "Bye!"
---"Hello":_:"a" -> 
+             "KILL_SERVICE" -> hPutStrLn hdl "Bye!"
+             "stop" -> hPutStrLn hdl "Bye!"
+             "quit" -> hPutStrLn hdl "Bye!"
+             "exit" -> hPutStrLn hdl "Bye!"
              -- else, continue looping.
-             _  -> broadcast (name ++ ": " ++ line) >> loop
-    killThread reader                      -- kill after the loop ends
-    broadcast ("<-- " ++ name ++ " left.") -- make a final broadcast
-    hClose hdl                             -- close the handle
+             _  -> broadcast (" wantAname !!: " ++ line) >>loop
+        killThread reader                      -- kill after the loop ends
+        broadcast ("<-- one left.") -- make a final broadcast
+        hClose hdl                             -- close the handle
